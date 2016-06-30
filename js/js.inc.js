067ffe4b06caf09e4423c7d7619ec6903c276221
pholpoem1 <jin.pholpoem@gmail.com>
2016-06-30 15:24:34 +0700

    edit

--- js/js.inc.js
+++ js/js.inc.js
@@ -1,6 +1,39 @@
-$(document).ready(function(){
-  $("#load_advance").click(function(){
-    $(".main").empty();
-    $("#main_adv").load("advance_search.html");
-  });
+$(document).ready(function() {
+
+    $("#author").click(function() {
+        $("#author_panel").slideToggle("slow");
+    });
+    $("#years").click(function() {
+        $("#years_panel").slideToggle("slow");
+    });
+    $("#title").click(function() {
+        $("#title_panel").slideToggle("slow");
+    });
+    $("#type_doc").click(function() {
+        $("#typeDoc_panel").slideToggle("slow");
+    });
+
 });
+
+function remove_attr(txt, vari) {
+    document.getElementById(vari).removeAttribute("href");
+    document.getElementById(vari).removeAttribute("onclick");
+    document.getElementById(vari+"-glyphicon").style.display = "inline";
+
+    //add childe
+    var node = document.createElement("LI");
+    node.setAttribute("id", vari+"_li");
+    var textnode = document.createTextNode(txt);
+    node.appendChild(textnode);
+    document.getElementById("navigator").appendChild(node);
+}
+
+function refhesh_attr(txt, vari) {
+    document.getElementById(vari).setAttribute("href", "javascript:void(0)");
+    document.getElementById(vari).setAttribute("onclick", "remove_attr('" + txt + "','" + vari + "');");
+    document.getElementById(vari+"-glyphicon").style.display = "none";
+
+    //remove childe
+    var item = document.getElementById(vari+"_li");
+    item.parentNode.removeChild(item);
+}
